a0f4c14a759604825ceec01805fe558897d7c2ff
Tim Lieberman <tim.lieberman@careerbuilder.com>
2016-09-17 23:04:55 -0400

    Adding bootstrap and pushing to head out

---
+++ ReputationBuilder/src/LandingPageBundle/Entity/Register.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace LandingPageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Register
+ *
+ * @ORM\Table(name="register")
+ * @ORM\Entity(repositoryClass="LandingPageBundle\Repository\RegisterRepository")
+ */
+class Register
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Register
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
